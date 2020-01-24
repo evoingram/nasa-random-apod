24ab0cd92209599bf6bc70d4ae8238e348b27b8f
Erica L. Ingram <inquiries@aquoco.co>
2020-01-23 16:52:12 -0800

    build version

---
+++ precache-manifest.2facd9d7b1048c5a7bb4bfc871049584.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b2268a83388c65b107d27f5a7253ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84589482fb7533640e77",
+    "url": "/static/css/main.bac97bac.chunk.css"
+  },
+  {
+    "revision": "105bc2fb2c22578b5d28",
+    "url": "/static/js/2.c7d15974.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c7d15974.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84589482fb7533640e77",
+    "url": "/static/js/main.f11c941c.chunk.js"
+  },
+  {
+    "revision": "c55054cc21f92083572b",
+    "url": "/static/js/runtime-main.0eec9a60.js"
+  },
+  {
+    "revision": "f2180a83f853056a77be05080a74fe5a",
+    "url": "/static/media/milkyWay.f2180a83.jpg"
+  },
+  {
+    "revision": "d75f66eebd2d16811286979706c5221a",
+    "url": "/static/media/rocket.d75f66ee.svg"
+  },
+  {
+    "revision": "93bb3dcbb8dfcd8789ba85b2c7b0b664",
+    "url": "/static/media/world.93bb3dcb.svg"
+  }
+]);
